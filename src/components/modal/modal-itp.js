51f849fa4ae3c138d31e712af06a7f340b71d326
vonncastro <vonn.castro@webriq.services>
2020-08-29 13:52:00 +0800

    new pages

--- src/components/modal/modal-itp.js
+++ src/components/modal/modal-itp.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Button, Modal, ModalHeader, ModalBody } from 'reactstrap'
+import itp from '../../images/itp-chart.png'
 
 const ModalITP = props => {
   const { className } = props
@@ -61,7 +62,12 @@ const ModalITP = props => {
             awareness and a security conscious culture will greatly assist in
             reducing threats for your organization.
           </p>
-
+          <img
+            src={itp}
+            alt="Penetration Testing"
+            className="img-fluid"
+            width="100%"
+          />
           <iframe
             title="Avail Our Services"
             src=""
